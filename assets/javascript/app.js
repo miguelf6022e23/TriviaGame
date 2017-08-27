3fec461e3734854123d9378df06712992e59bfe7
Miguel <miguelf6.022e23@gmail.com>
2017-08-27 19:06:51 -0400

    just need to add timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,25 +2,39 @@ var question;
 var index;
 var next;
 var time;
-var right;
-var wrong;
-function reset(){
-	console.log(data.length);
+var right = 0;
+var wrong = 0;
+var time;
+var reset = function(){
 	index = Math.floor(Math.random()*data.length);
 
-	next = false
+	next = false;
 
-	question = data[index]
-	disp()
+	question = data[index];
+	disp();
 }
 reset();
-console.log(question)
+console.log(question);
 
 function disp(){
-	$("#q").html(question.question)
-	$("#a").html(question.A)
-	$("#b").html(question.B)
-	$("#c").html(question.C)
-	$("#d").html(question.D)
+	$(":button").show()
+	$("#q").html(question.question);
+	$("#a").html(question.A);
+	$("#b").html(question.B);
+	$("#c").html(question.C);
+	$("#d").html(question.D);
+	$('#right').html("Number of Correct Answers: "+right);
+	$('#wrong').html("Number of Wrong Answers: "+wrong);
+}
 
+var play = function(){
+	if (this.value == question.answer){
+		$("#q").html("Correct!")
+		right++
+	} else{
+		$("#q").html("Wrong!")
+		wrong++
+	}
+	$(":button").hide();
+	setTimeout(reset,2000)
 }
