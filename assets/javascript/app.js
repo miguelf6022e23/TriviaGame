e33d258614deab9aa0ee0879c4e91bf234f451e8
Miguel <miguelf6.022e23@gmail.com>
2017-08-27 18:14:13 -0400

    Questions and answers now display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,26 @@
-console.log(data.length);
+var question;
+var index;
+var next;
+var time;
+var right;
+var wrong;
+function reset(){
+	console.log(data.length);
+	index = Math.floor(Math.random()*data.length);
+
+	next = false
+
+	question = data[index]
+	disp()
+}
+reset();
+console.log(question)
+
+function disp(){
+	$("#q").html(question.question)
+	$("#a").html(question.A)
+	$("#b").html(question.B)
+	$("#c").html(question.C)
+	$("#d").html(question.D)
+
+}
